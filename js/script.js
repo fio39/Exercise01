9bf650a244481b4715185c1ee59fddfb6b2542c8
fio39 <fio39@yandex.ru>
2020-05-31 21:07:24 +0200

    30-05-2020

--- js/script.js
+++ js/script.js
@@ -17,12 +17,28 @@ const appData = {
 let expenseItem;
 let expenseCost;
 
-expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
-expenseCost = prompt("Во сколько обойдется?");
 
-appData.expenses.expenseItem = expenseItem;
-appData.expenses.expenseCost = expenseCost;
+for(let i = 0; i < 2; i++) {
+    console.log('start i: ' + i);
+    expenseItem = prompt("Введите обязательную статью расходов в этом месяце");
+    expenseCost = prompt("Во сколько обойдется?");
 
-let moneyDay = appData.money / 30;
+    if( (typeof(expenseItem)) == 'string' && expenseItem != null &&
+        expenseItem != '' && expenseCost != '') {
 
-alert(moneyDay); 
+        appData.expenses[expenseItem] = expenseCost;
+        console.log('continue i: ' + i);
+
+    } else {
+        i -= 2
+        console.log('return i: ' + i);
+    }
+    console.log('finish i: ' + i);
+
+}
+
+
+appData.moneyDay = appData.money / 30;
+
+alert("Дневной заработок: " + appData.moneyDay); 
+console.log(appData);
---
+++ js/test.js
@@ -0,0 +1,6 @@
+console.log("Ёж" > "Ябл");
+console.log("ё".charCodeAt(0));
+console.log("я".charCodeAt(0));
+console.log("Ё".charCodeAt(0));
+console.log("Я".charCodeAt(0));
+console.log(0 || "" || undefined|| true);
